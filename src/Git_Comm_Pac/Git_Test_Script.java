9772b6a392e9a977286b6288d4e6711f141a9b82
Mohitkrdas <mkdbmd@gmail.com>
2020-09-11 12:06:59 +0530

    first commit Git_Test_Script added

---
+++ src/Git_Comm_Pac/Git_Test_Script.java
@@ -0,0 +1,28 @@
+package Git_Comm_Pac;
+
+import java.util.Scanner;
+
+public class Git_Test_Script
+{
+	public static void main(String [] args)
+	{
+		
+		String initial_String;
+		String reverse_String="";
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the string to reverse");
+		
+		initial_String=scan.nextLine();
+		int length=initial_String.length();
+		
+		for(int i=length-1;i>=0;i--)
+		{
+			reverse_String=reverse_String+initial_String.charAt(i);
+		}
+		  
+		System.out.println("Reversed string: "+reverse_String);
+  
+ 
+  
+	}
+}
