10291da062a7e25c55a7995876fb65b18fc6ae10
Mohitkrdas <mkdbmd@gmail.com>
2020-09-12 20:16:37 +0530

    Test Script 3

--- src/Git_Comm_Pac/Test_Script_3.java
+++ src/Git_Comm_Pac/Test_Script_3.java
@@ -5,6 +5,8 @@ public class Test_Script_3 {
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to Jumangi");
+	System.out.println("Welcome to Jumangi1");
+	System.out.println("Welcome to Jumangi2");
 
 	}
 
