8398ad6a03140a24bc432bc346b5e0b5f23069a0
Mohitkrdas <mkdbmd@gmail.com>
2020-09-12 20:19:25 +0530

    Revert "Test Script 3"
    This reverts commit 10291da062a7e25c55a7995876fb65b18fc6ae10.

--- src/Git_Comm_Pac/Test_Script_3.java
+++ src/Git_Comm_Pac/Test_Script_3.java
@@ -5,8 +5,6 @@ public class Test_Script_3 {
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to Jumangi");
-	System.out.println("Welcome to Jumangi1");
-	System.out.println("Welcome to Jumangi2");
 
 	}
 
